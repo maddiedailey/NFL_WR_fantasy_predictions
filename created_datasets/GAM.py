907c9cc562ed6991fb67ef2a33aef5cd5dca2e58
Maddie Dailey <maddiedailey@gmail.com>
2023-06-11 12:27:16 -0700

    model outlint

---
+++ created_datasets/GAM.py
@@ -0,0 +1,21 @@
+import sys
+import pygam
+from pygam import LinearGAM, s, f, te
+import pandas as pd
+import numpy as np
+import csv
+import matplotlib as plt
+from sklearn.model_selection import train_test_split
+
+
+def data_processing():
+    return X_train, X_test, Y_train, Y_test
+
+def main():
+
+    return
+
+
+if __name__ == '__main__':
+    main()
+
