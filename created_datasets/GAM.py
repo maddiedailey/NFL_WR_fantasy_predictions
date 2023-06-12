fa0f26738f5c0d75aecc482d6bcbd014e4a2e2f9
Maddie Dailey <maddiedailey@gmail.com>
2023-06-11 22:40:27 -0700

    plots

--- created_datasets/GAM.py
+++ created_datasets/GAM.py
@@ -2,9 +2,10 @@ import sys
 from pygam import LinearGAM, s, f, te
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+import csv
 
 DATA = "/Users/maddiedailey/Desktop/wr.csv"
 
@@ -29,7 +30,7 @@ def main():
     # remove categorical data
     data = data.drop("Name", axis = 1)
     data = data.drop("Week", axis = 1)
-    #data = data.drop("total_receiving_yards_allowed", axis=1)
+    # data = data.drop("total_receiving_yards_allowed", axis=1)
 
     # train two models for each of our performance metrics
     td_x_train, td_x_test, td_y_train, td_y_test = data_processing(data, "Receiving.TDs.x")
@@ -38,7 +39,9 @@ def main():
     print("Fitting Model...")
 
     td_gam = LinearGAM().fit(td_x_train, td_y_train)
-    #ry_gam = LinearGAM().fit(ry_x_train, ry_y_train)
+    ry_gam = LinearGAM().fit(ry_x_train, ry_y_train)
+
+    # OPTIMIZATION #####
 
     # Perform cross-validation for each df value
     # lam_values = [0.01, 0.1, 0.5, 0.6, 0.8]
@@ -53,8 +56,10 @@ def main():
     # optimal_lam = lam_values[np.argmin(cv_scores)]
     # print("Optimal Lambda:", optimal_lam)
 
-    ry_gam = LinearGAM(s(0) + te(1,3) + te(2,5) + s(4) + s(5) + s(6) + te(7,8) + s(9) + 
-                       s(10) + s(11) + s(12)).fit(ry_x_train, ry_y_train)
+    #ry_gam = LinearGAM(s(0) + te(1,3) + te(2,5) + s(4) + s(5) + s(6) + te(7,8) + s(9) + 
+    #                   s(10) + s(11) + s(12)).fit(ry_x_train, ry_y_train)
+
+    ######################
 
     print("TD Model: ")
     print(td_gam.summary())
@@ -71,6 +76,35 @@ def main():
     print(td_mse)
     print("Recieving Yards Model Performance: (MSE)")
     print(ry_mse)
+
+
+    # Create TD plots
+    # plt.scatter(td_y_test, td_y_pred, color='blue', alpha=0.5, label='TD Data Points')
+
+    # # Add a red line overlaying the plot
+    # plt.plot([min(td_y_test), max(td_y_test)], [min(td_y_test), max(td_y_test)], color='red', linestyle='-', linewidth=2, label='Ideal')
+    
+    # # Set labels and title
+    # plt.xlabel('True TD Values')
+    # plt.ylabel('Predicted TD Values')
+    # plt.title('Scatter Plot: True vs. Predicted')
+
+    # # Display the plot
+    # plt.show()
+
+    # Create RY plots
+    # plt.scatter(ry_y_test, ry_y_pred, color='blue', alpha=0.5, label='RY Data Points')
+
+    # # Add a red line overlaying the plot
+    # plt.plot([min(ry_y_test), max(ry_y_test)], [min(ry_y_test), max(ry_y_test)], color='red', linestyle='-', linewidth=2, label='Ideal')
+    
+    # # Set labels and title
+    # plt.xlabel('True RY Values')
+    # plt.ylabel('Predicted RY Values')
+    # plt.title('Scatter Plot: True vs. Predicted')
+
+    # # Display the plot
+    # plt.show()
 
 
 
